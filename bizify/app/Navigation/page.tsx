eef0abac44ad8e8693bbe22a9ce6ff2200cea915
Emma24968 <emnabs12@gmail.com>
2026-03-06 06:27:59 +0100

    Added the navigations

--- bizify/app/Navigation/page.tsx
+++ bizify/app/Navigation/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../../public/logo.svg'
 
 export default function page() {
   return (
     <div>
-        
+        <img src={logo} alt="logo" />
     </div>
   )
 }
